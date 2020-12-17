68fdc22464810c51b703a33e529cdbd6d09437de
Sachin Maheshwari <sachin.maheshwari@topcoder.com>
2020-12-17 15:28:16 +0530

    setting auth0 for prod env

--- web-assets/js/setupAuth0WithRedirect.js
+++ web-assets/js/setupAuth0WithRedirect.js
@@ -23,8 +23,8 @@ const qs = (function (a) {
 
 const authSetup = function () {
 
-    let domain = 'auth.topcoder-dev.com';
-    const clientId = 'BXWXUWnilVUPdN01t2Se29Tw2ZYNGZvH';
+    let domain = 'auth.topcoder.com';
+    const clientId = 'UW7BhsnmAQh0itl56g1jUPisBO9GoowD';
     const useLocalStorage = false;
     const useRefreshTokens = false;
     const v3JWTCookie = 'v3jwt';
@@ -38,7 +38,7 @@ const authSetup = function () {
     const utmSource = qs['utm_source'];
     const utmMedium = qs['utm_medium'];
     const utmCampaign = qs['utm_campaign'];
-    const loggerMode = "dev";
+    const loggerMode = "prod";
     const IframeLogoutRequestType = "LOGOUT_REQUEST";
     const enterpriseCustomers = ['zurich', 'cs'];
     const mode = qs['mode'] || 'signIn';
@@ -48,7 +48,7 @@ const authSetup = function () {
     if (utmSource &&
         (utmSource != 'undefined') &&
         (enterpriseCustomers.indexOf(utmSource) > -1)) {
-        domain = "topcoder-dev.auth0.com";
+        domain = "topcoder.auth0.com";
         returnAppUrl += '&utm_source=' + utmSource;
     }
 
