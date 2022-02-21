08578b319b980d70b25e15d994ed4aa8b07ec133
Sachin Maheshwari <sachin.maheshwari@topcoder.com>
2022-02-21 17:17:54 +0530

    query param issue

--- web-assets/js/setupAuth0WithRedirect.js
+++ web-assets/js/setupAuth0WithRedirect.js
@@ -671,7 +671,7 @@ const authSetup = function () {
                 hostdomain = "." + location.hostname.split('.').reverse()[1] +
                     "." + location.hostname.split('.').reverse()[0];
             }
-            const prefixArray = ['apps', 'software'];
+            const prefixArray = ['apps', 'software', 'herokuapp'];
             if (hostdomain && value) {
                 for (let i = 0; i < prefixArray.length; i++) {
                     if (value.indexOf(prefixArray[i] + hostdomain) > -1) {
