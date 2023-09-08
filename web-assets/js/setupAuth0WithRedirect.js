eafbee9032663fe05c1df9f14c540e2ee345dfe3
eisbilir <emre.isbilir@gmail.com>
2023-09-08 16:10:02 +0300

    handle OR returnUrl

--- web-assets/js/setupAuth0WithRedirect.js
+++ web-assets/js/setupAuth0WithRedirect.js
@@ -656,7 +656,7 @@ const authSetup = function () {
                 hostdomain = "." + location.hostname.split('.').reverse()[1] +
                     "." + location.hostname.split('.').reverse()[0];
             }
-            const prefixArray = ['apps'];
+            const prefixArray = ['apps', 'software'];
             if (hostdomain && value) {
                 for (let i = 0; i < prefixArray.length; i++) {
                     if (value.indexOf(prefixArray[i] + hostdomain) > -1) {
